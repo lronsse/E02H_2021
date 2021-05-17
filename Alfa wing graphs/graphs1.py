871cb8535fe94b6e294b4df6f38b6f1cc13d2da5
ja-key <J.Korycki@student.tudelft.nl>
2021-05-17 15:42:31 +0200

    more grefs

--- Alfa wing graphs/graphs1.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel(r'dataset.xlsx', sheet_name='LOAD & DISPLACEMENT')
-print(df)
---
+++ Alfa wing graphs/vertical displacement.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_excel(r'dataset.xlsx', sheet_name='LOAD & DISPLACEMENT')
+data = df[["LOADCELL", "displacement 1", "displacement 2"]]
+print(data)
+
+xdata1 = data["displacement 1"]
+xdata2 = data["displacement 2"]
+ydata = -data["LOADCELL"]
+
+fig = plt.figure()
+ax = plt.axes()
+
+ax.plot(xdata1, ydata, label="Sensor 1")
+ax.plot(xdata2, ydata, label="Sensor 2")
+plt.legend()
+# axs[0].set_title("Displacement 1 vs loadcell")
+ax.set_xlabel("Displacement [mm]")
+ax.set_ylabel("Load [N]")
+
+"""
+axs[1].plot(xdata2, ydata)
+# axs[1].set_title("Displacement 2 vs loadcell")
+axs[1].set_xlabel("Displacement 2 [mm]")
+axs[1].set_ylabel("Load [N]")
+"""
+fig.tight_layout()
+plt.show()
