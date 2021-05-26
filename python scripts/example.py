62077ad09a821288c9ac12f6f3ff1d953d87a82f
ja-key <J.Korycki@student.tudelft.nl>
2021-05-26 12:45:55 +0200

    yes

--- python scripts/example.py
+++ python scripts/example.py
@@ -6,24 +6,24 @@ top_spacing = 10.8
 side_spacing = 50
 rowspacing = 75.68
 
-rivetsperrow = [29, 29, 28, 27, 26, 24]
+rivetsperrow1 = [29, 29, 28, 27, 26, 24]
 
 xcords, ycords = [], []
 
 # stringer holes
 for i in range(nstringers):
-    for j in range(rivetsperrow[i]):
-        ycords.append(side_spacing+j*rivet_spacing1)
+    for j in range(rivetsperrow1[i]):
+        ycords.append(float(side_spacing+j*rivet_spacing1))
         xcords.append(round(top_spacing+i*75.68, 4))
 
 # rib holes
-rivetsperrow = 10
+rivetsperrow2 = 10
 top_spacing = 33.64
-rowys = [230, 420, 730, 1020]
-spacing = 0
+rowys = [230.0, 420.0, 730.0, 1020.0]
+spacing = 0.0
 for i in range(3):
-    for j in range(rivetsperrow):
-        ycords.append(round(230+rowys[i], 4))
+    for j in range(rivetsperrow2):
+        ycords.append(round(230.0+rowys[i], 4))
         if j % 2 == 0:
             spacing += top_spacing+j*30
         else:
@@ -35,3 +35,5 @@ data = {"xcords": xcords, "ycords": ycords}
 frame = pd.DataFrame(data)
 frame.to_csv("C1.csv", header=False, index=False)
 print(frame)
+frame.set_option("display.max.columns", None)
+frame.head()
